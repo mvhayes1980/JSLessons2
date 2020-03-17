c421beac0db32607066cccd23bee2ecb541b63c2
MICHAEL V HAYES <michaelvhayes@michaels-mbp-2.home>
2020-03-17 14:23:46 -0400

    initial

---
+++ HTMLChallenges/JSFundamentals/1-GrammarAndTypes/02-declarations.js
@@ -0,0 +1,29 @@
+
+var a = 1;
+    var     b       =     2; 
+//  (1)     (2)    (3)   (4)
+
+/*
+(1) var keyword (denotes a variable)
+(2) variable name (cannot be a reserved word, or start with a number)
+(3) assigment operator
+(4) value put inside the variable
+*/
+
+//variable declaration
+var stream;
+
+//variable initialization
+stream = true;
+
+//variable re-initialization
+stream = false;
+
+//var, let, const
+
+let today = "great";
+const efa = "School";
+
+console.log(today,efa);
+
+const testing;
---
+++ HTMLChallenges/JSFundamentals/1-GrammarAndTypes/03-types.js
@@ -0,0 +1,74 @@
+//Boolean
+let on= true;
+console.log(on);
+
+let off = false;
+console.log(off);
+
+//Null
+let empty = null;//no value, but not undefined; simply holds nothing; it has a nothing value
+
+/*
+Null and undefined are different
+*/
+
+//Undefined
+let undef = undefined;//same as let undef -- declaration, no initialization
+
+//Numbers
+let degrees = 90;
+let precise = 999999999999999;
+
+console.log(precise);
+
+let toomuch = 9999999999999999n;//PROBLEM with JS rounding after 16
+
+console.log(toomuch);
+
+let notQuite = 0.2 + 0.1;
+console.log(notQuite);
+
+console.log ( typeof toomuch);
+
+let stringOne = 'single quotes';
+let stringTwo = "double quotes";
+let stringThree = `backticks`;
+
+//Addition
+let first = 1050 + 10;
+//Concatenation
+let second = "1050" + "10";
+
+
+console.log(first, second);
+
+let third = "1050" + 10;
+
+console.log(third);
+console.log(typeof third);
+
+/*
+CHALLENGE
+Set 7 (or 8) variables:
+firstName
+lastName
+houseNumber
+aptNumber (if applicable)
+street
+city
+state
+zipcode
+Set each variable to its respective type.
+console.log your whole address (with the space between variable).
+Collapse
+*/
+const firstName = "Mike";
+let lastName = "Hayes";
+var houseNumber = 5406;
+var street = "Kenwood Ave.";
+var aptNumber = 1;
+var city = "Indianapolis";
+var state = "Indiana";
+var zipcode = 46208;    
+
+console.log(firstName + ` ` + lastName + ` ` + houseNumber + ` ` + aptNumber + ` ` + street + ` ` + city + ` ` + state + ` ` + zipcode);
